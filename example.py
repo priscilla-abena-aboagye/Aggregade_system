df40f51a10459d8bcb942d8ff8706ef03bfda035
Abena <priscillaabenaaboagye@gmail.com>
2025-01-18 22:45:01 +0000

    Update example.py

--- example.py
+++ example.py
@@ -1,11 +1,20 @@
 print(input("Enter your name: "))
+score = 0
+allSubject = []
+# while True:
+#     subject = input("Enter the subject (press q to quit): ").lower()
+#     # grade = input("Enter the grade (press q to quit): ").lower()
+#     if subject == "q":
+#         break
+#     allSubject.append(subject)
+#     # elif grade == "q":
+#     #     break
+# # score = int(score)
 
-while True:
-    subject = input("Enter the subject (press q to quit): ").lower()
-    grade = input("Enter the grade (press q to quit): ").lower()
-    if subject == "q":
-        break
-    elif grade == "q":
-        break
+# print(allSubject)
 
+foodStuffs = ['mango', 'guava', 'pear']
+prices = [10, 11, 200]
 
+myDictionary = dict(zip(foodStuffs, prices))
+print(myDictionary)
