3af2f2baedb3481307f5faa75d3b8262660fdb55
Abena <priscillaabenaaboagye@gmail.com>
2025-01-18 20:45:41 +0000

    An Aggregate system

---
+++ example.py
@@ -0,0 +1,11 @@
+print(input("Enter your name: "))
+
+while True:
+    subject = input("Enter the subject (press q to quit): ").lower()
+    grade = input("Enter the grade (press q to quit): ").lower()
+    if subject == "q":
+        break
+    elif grade == "q":
+        break
+
+
